a7e6d6553e16b235e397e7fa59e006877daad315
changwon son <thswave@gmail.com>
2013-11-26 23:34:22 +0900

    modify password find function

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -24,20 +24,12 @@ class RegistrationsController < ApplicationController
   end
 
   def update
-    # account_update_params = params[:user]
-
-    # @user = User.find_by_username_and_student_id(params[:user][:username],
-    #   params[:user][:student_id])
     @user = User.where( 
-      username: params[:user][:username],
       student_id: params[:user][:student_id],
       realname: params[:user][:realname]
       ).first
-    # ,
-    #   
-    logger.debug "====#{@user}="
+    
     account_update_params = params[:user]
-    account_update_params.delete("username")
     account_update_params.delete("realname")
     account_update_params.delete("student_id")
     
